bce515c42a0a569cbab54ed5f79f284dc790cfc4
ZabihullahNooriWardak <115200857+ZabihullahNooriWardak@users.noreply.github.com>
2023-07-06 18:50:03 +0430

    about section added

---
+++ about.js
@@ -0,0 +1,7 @@
+
+//code for hiding popUp in about page
+
+let burgerMenu=document.getElementById('bx');
+burgerMenu.addEventListener('click',function(){
+document.querySelector('.burger-menue').style.display='inline';
+});

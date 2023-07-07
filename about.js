cf0d5821cf4837d807960d6dbd140e1769231e71
ZabihullahNooriWardak <115200857+ZabihullahNooriWardak@users.noreply.github.com>
2023-07-07 07:48:42 +0430

    burger menu pic fixed for about page

--- about.js
+++ about.js
@@ -3,8 +3,10 @@
 const burgerMenu = document.getElementById('bx');
 burgerMenu.addEventListener('click', () => {
   document.querySelector('.burger-menue').style.display = 'inline';
+  document.querySelector('.images-container').style.display='none';
 });
 
 document.getElementById('zabih').addEventListener('click', () => {
   document.querySelector('.burger-menue').style.display = 'none';
+  document.querySelector('.images-container').style.display='inline';
 });
